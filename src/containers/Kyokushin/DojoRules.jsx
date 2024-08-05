c110cce5dd14a4e53fe6b8ff7e7ca29344e77d7f
Juan Venegas Fuenzalida <j.venegasfuenzalida@gmail.com>
2024-08-05 17:16:06 -0400

    fix and compact dojokun and dojorules section

--- src/containers/Kyokushin/DojoRules.jsx
+++ src/containers/Kyokushin/DojoRules.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { dojoRules } from '../../data/dojorules'
+import React from "react";
+import { dojoRules } from "../../data/dojorules";
 const DojoRules = () => {
   return (
     <div className="dojo-info-section">
-          <h2 className="header">Reglas del Dojo</h2>
-          <div className="dojo-rules">
-            {dojoRules.map((rule, index) => (
-              <div className="dojo-rule" key={index}>
-                <img
-                  src={rule.image}
-                  alt={`Regla ${index + 1}`}
-                  className="dojo-rule-image"
-                />
-                <p className="dojo-rule-text">{rule.text}</p>
-              </div>
-            ))}
+      <h2 className="header">Reglas del Dojo</h2>
+      <div className="dojo-rules">
+        {dojoRules.map((rule, index) => (
+          <div className="dojo-rule" key={index}>
+            <img
+              src={rule.image}
+              alt={`Regla ${index + 1}`}
+              className="dojo-rule-image"
+            />
+            <p className="dojo-rule-text">{rule.text}</p>
           </div>
-        </div>
-  )
-}
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default DojoRules
+export default DojoRules;
